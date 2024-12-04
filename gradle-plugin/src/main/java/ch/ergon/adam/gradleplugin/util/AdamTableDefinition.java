774e31c02e5ab7e6ad7ee3b3dde99e6fc0be208d
Jonas Matter <jonas.matter@ergon.ch>
2024-12-04 16:29:03 +0100

    support arrays in metamodel generation

--- gradle-plugin/src/main/java/ch/ergon/adam/gradleplugin/util/AdamTableDefinition.java
+++ gradle-plugin/src/main/java/ch/ergon/adam/gradleplugin/util/AdamTableDefinition.java
@@ -1,6 +1,5 @@
 package ch.ergon.adam.gradleplugin.util;
 
-import ch.ergon.adam.core.db.schema.DataType;
 import ch.ergon.adam.core.db.schema.Field;
 import ch.ergon.adam.core.db.schema.Table;
 import org.jooq.Name;
@@ -33,57 +32,69 @@ public class AdamTableDefinition extends AbstractTableDefinition {
 
     private DataTypeDefinition dataTypeDefinition(Field field) {
         return new DefaultDataTypeDefinition(getDatabase(), getSchema(),
-            typeName(field.getDataType()), field.getLength(), field.getPrecision(), field.getScale(),
+            typeName(field), field.getLength(), field.getPrecision(), field.getScale(),
             field.isNullable(), field.getDefaultValue(), getUserType(field));
     }
 
-    private String typeName(DataType type) {
-        var jooqType = switch (type) {
-            case BIGINT -> SQLDataType.BIGINT;
-            case BIGINTUNSIGNED -> SQLDataType.BIGINTUNSIGNED;
-            case BINARY -> SQLDataType.BINARY;
-            case BIT -> SQLDataType.BIT;
-            case BLOB -> SQLDataType.BLOB;
-            case BOOLEAN -> SQLDataType.BOOLEAN;
-            case CHAR -> SQLDataType.CHAR;
-            case CLOB -> SQLDataType.CLOB;
-            case DATE -> SQLDataType.DATE;
-            case DECIMAL -> SQLDataType.DECIMAL;
-            case DECIMAL_INTEGER -> SQLDataType.DECIMAL_INTEGER;
-            case DOUBLE -> SQLDataType.DOUBLE;
-            case ENUM -> SQLDataType.VARCHAR;
-            case FLOAT -> SQLDataType.FLOAT;
-            case INTEGER -> SQLDataType.INTEGER;
-            case INTEGERUNSIGNED -> SQLDataType.INTEGERUNSIGNED;
-            case INTERVALDAYTOSECOND -> SQLDataType.INTERVALDAYTOSECOND;
-            case INTERVALYEARTOMONTH -> SQLDataType.INTERVALYEARTOMONTH;
-            case INTERVALYEARTOSECOND -> SQLDataType.INTERVAL;
-            case LOCALDATE -> SQLDataType.LOCALDATE;
-            case LOCALDATETIME -> SQLDataType.LOCALDATETIME;
-            case LOCALTIME -> SQLDataType.LOCALTIME;
-            case LONGNVARCHAR -> SQLDataType.LONGNVARCHAR;
-            case LONGVARBINARY -> SQLDataType.LONGVARBINARY;
-            case LONGVARCHAR -> SQLDataType.LONGVARCHAR;
-            case NCHAR -> SQLDataType.NCHAR;
-            case NCLOB -> SQLDataType.NCLOB;
-            case NUMERIC -> SQLDataType.NUMERIC;
-            case NVARCHAR -> SQLDataType.NVARCHAR;
-            case OFFSETDATETIME -> SQLDataType.OFFSETDATETIME;
-            case OFFSETTIME -> SQLDataType.OFFSETTIME;
-            case REAL -> SQLDataType.REAL;
-            case SMALLINT -> SQLDataType.SMALLINT;
-            case SMALLINTUNSIGNED -> SQLDataType.SMALLINTUNSIGNED;
-            case TIME -> SQLDataType.TIME;
-            case TIMESTAMP -> SQLDataType.TIMESTAMP;
-            case TIMESTAMPWITHTIMEZONE -> SQLDataType.TIMESTAMPWITHTIMEZONE;
-            case TIMEWITHTIMEZONE -> SQLDataType.TIMEWITHTIMEZONE;
-            case TINYINT -> SQLDataType.TINYINT;
-            case TINYINTUNSIGNED -> SQLDataType.TINYINTUNSIGNED;
-            case UUID -> SQLDataType.UUID;
-            case VARBINARY -> SQLDataType.VARBINARY;
-            case VARCHAR -> SQLDataType.VARCHAR;
-        };
-        return jooqType.getTypeName();
+	private String typeName(Field field) {
+		var jooqType = switch (field.getDataType()) {
+		case BIGINT -> SQLDataType.BIGINT;
+		case BIGINTUNSIGNED -> SQLDataType.BIGINTUNSIGNED;
+		case BINARY -> SQLDataType.BINARY;
+		case BIT -> SQLDataType.BIT;
+		case BLOB -> SQLDataType.BLOB;
+		case BOOLEAN -> SQLDataType.BOOLEAN;
+		case CHAR -> SQLDataType.CHAR;
+		case CLOB -> SQLDataType.CLOB;
+		case DATE -> SQLDataType.DATE;
+		case DECIMAL -> SQLDataType.DECIMAL;
+		case DECIMAL_INTEGER -> SQLDataType.DECIMAL_INTEGER;
+		case DOUBLE -> SQLDataType.DOUBLE;
+		case ENUM -> SQLDataType.VARCHAR;
+		case FLOAT -> SQLDataType.FLOAT;
+		case INTEGER -> SQLDataType.INTEGER;
+		case INTEGERUNSIGNED -> SQLDataType.INTEGERUNSIGNED;
+		case INTERVALDAYTOSECOND -> SQLDataType.INTERVALDAYTOSECOND;
+		case INTERVALYEARTOMONTH -> SQLDataType.INTERVALYEARTOMONTH;
+		case INTERVALYEARTOSECOND -> SQLDataType.INTERVAL;
+		case LOCALDATE -> SQLDataType.LOCALDATE;
+		case LOCALDATETIME -> SQLDataType.LOCALDATETIME;
+		case LOCALTIME -> SQLDataType.LOCALTIME;
+		case LONGNVARCHAR -> SQLDataType.LONGNVARCHAR;
+		case LONGVARBINARY -> SQLDataType.LONGVARBINARY;
+		case LONGVARCHAR -> SQLDataType.LONGVARCHAR;
+		case NCHAR -> SQLDataType.NCHAR;
+		case NCLOB -> SQLDataType.NCLOB;
+		case NUMERIC -> SQLDataType.NUMERIC;
+		case NVARCHAR -> SQLDataType.NVARCHAR;
+		case OFFSETDATETIME -> SQLDataType.OFFSETDATETIME;
+		case OFFSETTIME -> SQLDataType.OFFSETTIME;
+		case REAL -> SQLDataType.REAL;
+		case SMALLINT -> SQLDataType.SMALLINT;
+		case SMALLINTUNSIGNED -> SQLDataType.SMALLINTUNSIGNED;
+		case TIME -> SQLDataType.TIME;
+		case TIMESTAMP -> SQLDataType.TIMESTAMP;
+		case TIMESTAMPWITHTIMEZONE -> SQLDataType.TIMESTAMPWITHTIMEZONE;
+		case TIMEWITHTIMEZONE -> SQLDataType.TIMEWITHTIMEZONE;
+		case TINYINT -> SQLDataType.TINYINT;
+		case TINYINTUNSIGNED -> SQLDataType.TINYINTUNSIGNED;
+		case UUID -> SQLDataType.UUID;
+		case VARBINARY -> SQLDataType.VARBINARY;
+		case VARCHAR -> SQLDataType.VARCHAR;
+		};
+
+        String typeName = jooqType.getTypeName();
+        if (field.isArray()) {
+            return getArrayTypeName(typeName);
+        }
+		return typeName;
+	}
+
+    /**
+     * Keep in sync with default case in {@link org.jooq.meta.AbstractDatabase#isArrayType(String dataType)}.
+     */
+    private String getArrayTypeName(String typeName) {
+        return typeName + " ARRAY";
     }
 
     private boolean isIdentity(Field field) {
